3a06788e6477a0569acc7250ab469586c152d170
Artem <131267822+ArtemChuprak@users.noreply.github.com>
2023-05-26 16:48:27 +0300

    Add files via upload

---
+++ cust-swiper.js
@@ -0,0 +1,28 @@
+let swiper;
+let init = false;
+
+function swiperCard() {
+	if (window.innerWidth <= 760) {
+		if (!init) {
+			init = true;
+
+			swiper = new Swiper(".swiper", {
+				loop: true,
+				pagination: {
+					el: ".swiper-pagination",
+				},
+				slidesPerView: 1.2,
+				spaceBetween: 5,
+			});
+		}
+	} else if (init) {
+		if (typeof swiper !== "undefined") {
+			swiper.destroy();
+		}
+
+		init = false;
+	}
+}
+
+swiperCard();
+window.addEventListener("resize", swiperCard);
